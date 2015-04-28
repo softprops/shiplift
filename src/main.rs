aadff518328583da5b878c6d87985ffb4046df20
softprops <d.tangren@gmail.com>
2015-04-28 00:04:18 -0400

    the lastest hyper works

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use shiplift::Docker;
 
 fn main() {
   let mut docker = Docker::new();
+  let info = docker.info().unwrap();
+  println!("info -> {:?}", info);
   let read = docker.images().create("redis:3.0.0").unwrap();
   for e in jed::Iter::new(read) {
     println!("\n -> {:?}", e);
