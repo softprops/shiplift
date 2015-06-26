a2c9a6b8206616cfbc3c713bee062ab332400ada
softprops <d.tangren@gmail.com>
2015-06-26 00:45:40 -0400

    workin on examples for stats and export

--- src/bin/shiplft.rs
+++ src/bin/shiplft.rs
@@ -1,11 +1,24 @@
 extern crate shiplift;
 
 use shiplift::Docker;
+use std::io::prelude::*;
+use std::io::copy;
+use std::fs::OpenOptions;
 
 fn main() {
   let mut docker = Docker::new();
   println!("{:?}", docker.info().unwrap());
-  //for e in docker.events().get().unwrap() {
-  //  println!("{:?}", e);
+
+  let mut export = OpenOptions::new().write(true).create(true).open("export.tgz").unwrap();
+  let mut images = docker.images();
+  let mut exported = images.get("nginx").export().unwrap();
+  println!("copying");
+  copy(&mut exported, &mut export).unwrap();
+  println!("copied");
+
+  //let mut containers = docker.containers();
+  //let stats = containers.get("f527f9be52b2").stats();
+  //for s in stats.unwrap() {
+  //  println!("{:?}", s);
   //}
 }
