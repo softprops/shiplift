536b1c4312ad9e2e03ca403b260d61d709d20491
softprops <d.tangren@gmail.com>
2016-01-03 19:43:00 -0500

    more examples

---
+++ examples/containerinspect.rs
@@ -0,0 +1,15 @@
+extern crate shiplift;
+
+use shiplift::Docker;
+use std::env;
+
+fn main() {
+    let docker = Docker::new();
+    if let Some(id) = env::args().nth(1) {
+        let container = docker.containers()
+            .get(&id)
+            .inspect()
+            .unwrap();
+        println!("{:?}", container);
+    }
+}
---
+++ examples/imageinspect.rs
@@ -0,0 +1,15 @@
+extern crate shiplift;
+
+use shiplift::Docker;
+use std::env;
+
+fn main() {
+    let docker = Docker::new();
+    if let Some(id) = env::args().nth(1) {
+        let image = docker.images()
+            .get(&id)
+            .inspect()
+            .unwrap();
+        println!("{:?}", image);
+    }
+}
