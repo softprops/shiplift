4c0310779329f4d8ab5e29a8a17a1af5f1ba0e6b
softprops <d.tangren@gmail.com>
2016-01-18 13:46:35 -0500

    reduce number of unwrap calls

--- src/tarball.rs
+++ src/tarball.rs
@@ -6,20 +6,20 @@ use std::path::{Path, MAIN_SEPARATOR};
 use std::io::{self, Write, Read};
 use tar::Archive;
 
-// todo: this is pretty involved. factor this into its own crate
+// todo: this is pretty involved. (re)factor this into its own crate
 pub fn dir<W>(buf: W, path: &str) -> io::Result<()> where W: Write {
     let archive = Archive::new(GzEncoder::new(buf, Compression::Best));
-    fn bundle(dir: &Path, cb: &Fn(&Path), bundle_dir: bool) -> io::Result<()> {
+    fn bundle<F>(dir: &Path, f: &F, bundle_dir: bool) -> io::Result<()> where F: Fn(&Path) -> io::Result<()> {
         if try!(fs::metadata(dir)).is_dir() {
             if bundle_dir {
-                cb(&dir);
+                try!(f(&dir));
             }
             for entry in try!(fs::read_dir(dir)) {
                 let entry = try!(entry);
                 if try!(fs::metadata(entry.path())).is_dir() {
-                    try!(bundle(&entry.path(), cb, true));
+                    try!(bundle(&entry.path(), f, true));
                 } else {
-                    cb(&entry.path().as_path());
+                    try!(f(&entry.path().as_path()));
                 }
             }
         }
@@ -27,20 +27,25 @@ pub fn dir<W>(buf: W, path: &str) -> io::Result<()> where W: Write {
     }
 
     {
-        let base_path = Path::new(path).canonicalize().unwrap();
+        let base_path = try!(Path::new(path).canonicalize());
+        // todo: don't unwrap
         let mut base_path_str = base_path.to_str().unwrap().to_owned();
-        if base_path_str.chars().last().unwrap() != MAIN_SEPARATOR {
-            base_path_str.push(MAIN_SEPARATOR)
+        if let Some(last) = base_path_str.chars().last() {
+            if last != MAIN_SEPARATOR {
+                base_path_str.push(MAIN_SEPARATOR)
+            }
         }
 
         let append = |path: &Path| {
-            let canonical = path.canonicalize().unwrap();
+            let canonical = try!(path.canonicalize());
+            // todo: don't unwrap
             let relativized = canonical.to_str().unwrap().trim_left_matches(&base_path_str[..]);
             if path.is_dir() {
-                archive.append_dir(Path::new(relativized), &canonical).unwrap();
+                try!(archive.append_dir(Path::new(relativized), &canonical))
             } else {
-                archive.append_file(Path::new(relativized), &mut File::open(&canonical).unwrap()).unwrap();
+                try!(archive.append_file(Path::new(relativized), &mut try!(File::open(&canonical))))
             }
+            Ok(())
         };
         try!(bundle(Path::new(path), &append, false));
         try!(archive.finish());
