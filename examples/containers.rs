5440fe805465fec65738452b345ff8b5e8282b76
Hubert Banaszewski <hubert.banaszewski@golem.network>
2018-07-11 16:13:19 +0200

    Fixes

--- examples/containers.rs
+++ examples/containers.rs
@@ -5,7 +5,6 @@ use shiplift::Docker;
 
 fn main() {
     let docker = Docker::new(None).unwrap();
-    let docker = Docker::new();
     for c in docker.containers().list(&Default::default()).unwrap() {
         println!("container -> {:?}", c)
     }
