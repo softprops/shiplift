6a8da413a5a41086fcbc4f0ccc843f3a3b0c9308
Hubert Banaszewski <hubert.banaszewski@golem.network>
2018-07-17 19:53:42 +0200

    Implementation of GET archive request

---
+++ examples/archiveget.rs
@@ -0,0 +1,25 @@
+extern crate shiplift;
+
+use shiplift::{Docker, ContainerArchiveOptions};
+use std::env;
+
+// args:
+//      container
+//      docker container directory path
+//      local file/directory path
+fn main() {
+    let docker = Docker::new(None).unwrap();
+
+    if env::args().len() < 3 {
+        return;
+    }
+
+    let options = ContainerArchiveOptions::builder()
+        .local_path(env::args().nth(3).unwrap())
+        .path(env::args().nth(2).unwrap())
+        .build();
+
+
+    let id = env::args().nth(1).unwrap();
+    docker.containers().get(&id).archive_push(&options);
+}
