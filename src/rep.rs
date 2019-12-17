906a18d2fc85f9ef648ebd54fa1702a7bca736d0
colvin <39858617+colvin@users.noreply.github.com>
2019-12-17 14:53:53 -0500

    order version fields according to api docs

--- src/rep.rs
+++ src/rep.rs
@@ -401,10 +401,10 @@ pub struct Top {
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct Version {
+    pub version: String,
     pub api_version: String,
     #[serde(rename = "MinAPIVersion")]
     pub min_api_version: String,
-    pub version: String,
     pub git_commit: String,
     pub go_version: String,
     pub os: String,
