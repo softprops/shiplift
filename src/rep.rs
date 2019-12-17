477081e9c95287542e5891f7807278240e28eb18
colvin <39858617+colvin@users.noreply.github.com>
2019-12-17 14:41:42 -0500

    expand the fields of Version
    Expands `shiplift::rep::Version` to contain fields for the following
    elements of the `/version` API:
    
    - `min_api_version` (`MinAPIVersion`)
    - `os` (`Os`)
    - `arch` (`Arch`)
    - `kernel_version` (`KernelVersion`)
    - `build_time` (`BuildTime`)
    
    Adds a new example program to display version information.

--- src/rep.rs
+++ src/rep.rs
@@ -402,9 +402,18 @@ pub struct Top {
 #[serde(rename_all = "PascalCase")]
 pub struct Version {
     pub api_version: String,
+    #[serde(rename = "MinAPIVersion")]
+    pub min_api_version: String,
     pub version: String,
     pub git_commit: String,
     pub go_version: String,
+    pub os: String,
+    pub arch: String,
+    pub kernel_version: String,
+    #[cfg(feature = "chrono")]
+    pub build_time: DateTime<Utc>,
+    #[cfg(not(feature = "chrono"))]
+    pub build_time: String,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
