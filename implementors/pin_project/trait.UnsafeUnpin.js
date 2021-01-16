7439744bd45e82a89e781702acc313b1426b9c22
softprops <softprops@users.noreply.github.com>
2021-01-16 00:25:54 +0000

    deploy: 71a2d687187b7cc9f7954cbc041e3dd34349d0aa

--- implementors/pin_project/trait.UnsafeUnpin.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["shiplift"] = [{"text":"impl&lt;'a&gt; UnsafeUnpin for Multiplexer&lt;'a&gt;","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
