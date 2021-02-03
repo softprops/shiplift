ce78969a310b844c57db16f2a6be6b2a3e885d86
wojciechkepka <wojtek.kepka@protonmail.com>
2021-02-03 20:37:48 +0100

    Add example how to inspect an exec instance

---
+++ examples/execinspect.rs
@@ -0,0 +1,36 @@
+use futures::StreamExt;
+use shiplift::{Docker, ExecContainerOptions};
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let docker = Docker::new();
+    let containers = docker.containers();
+
+    let id = env::args()
+        .nth(1)
+        .expect("You need to specify a container id");
+
+    let container = containers.get(&id);
+
+    let opts = ExecContainerOptions::builder()
+        .cmd(vec!["echo", "1"])
+        .attach_stdout(true)
+        .attach_stderr(true)
+        .build();
+
+    let mut exec_id = String::new();
+    if let Some(_) = container.exec_with_id(&opts, &mut exec_id).next().await {
+        //handle output...
+    }
+
+    // Inspect exec
+    match container.exec_inspect(&exec_id).await {
+        Ok(exec_info) => {
+            println!("{:#?}", exec_info);
+            //exit code
+            let _exit_code = exec_info.exit_code;
+        }
+        Err(e) => eprintln!("{:?}", e),
+    }
+}
