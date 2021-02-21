41f3ddfa14bb05b14e88b2acf184d2f99d149134
softprops <softprops@users.noreply.github.com>
2021-02-21 00:52:21 +0000

    deploy: 9c72d3aacb675c5bcff238ce4996dbbe138e11c8

--- implementors/core/convert/trait.AsRef.js
+++ implementors/core/convert/trait.AsRef.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["shiplift"] = [{"text":"impl AsRef&lt;Vec&lt;u8&gt;&gt; for TtyChunk","synthetic":false,"types":[]}];
+implementors["shiplift"] = [{"text":"impl AsRef&lt;Vec&lt;u8, Global&gt;&gt; for TtyChunk","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
