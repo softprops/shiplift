9c77d6c01b518cd710718d0bb43c7c8f6b419dd4
Matthias Beyer <mail@beyermatthias.de>
2021-09-12 19:24:21 +0200

    Move helper function out of function body
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/tarball.rs
+++ src/tarball.rs
@@ -15,29 +15,6 @@ where
     W: Write,
 {
     let mut archive = Builder::new(GzEncoder::new(buf, Compression::best()));
-    fn bundle<F>(
-        dir: &Path,
-        f: &mut F,
-        bundle_dir: bool,
-    ) -> io::Result<()>
-    where
-        F: FnMut(&Path) -> io::Result<()>,
-    {
-        if fs::metadata(dir)?.is_dir() {
-            if bundle_dir {
-                f(&dir)?;
-            }
-            for entry in fs::read_dir(dir)? {
-                let entry = entry?;
-                if fs::metadata(entry.path())?.is_dir() {
-                    bundle(&entry.path(), f, true)?;
-                } else {
-                    f(&entry.path().as_path())?;
-                }
-            }
-        }
-        Ok(())
-    }
 
     {
         let base_path = Path::new(path).canonicalize()?;
@@ -67,5 +44,29 @@ where
     }
     archive.finish()?;
 
+    Ok(())
+}
+
+fn bundle<F>(
+    dir: &Path,
+    f: &mut F,
+    bundle_dir: bool,
+) -> io::Result<()>
+where
+    F: FnMut(&Path) -> io::Result<()>,
+{
+    if fs::metadata(dir)?.is_dir() {
+        if bundle_dir {
+            f(&dir)?;
+        }
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            if fs::metadata(entry.path())?.is_dir() {
+                bundle(&entry.path(), f, true)?;
+            } else {
+                f(&entry.path().as_path())?;
+            }
+        }
+    }
     Ok(())
 }
